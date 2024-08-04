12b434495d2859a1689bb8d6d853946c430a664c
Raka-schools <yrgreat2@gmail.com>
2024-08-04 12:46:32 +0530

    Sum of Digits of a Number
    I have used split function to split the string of numbers entered. After this, map function has been used to create an array of real number digits.

---
+++ Sum of Digits of a Number/sumOfDigits.js
@@ -0,0 +1,12 @@
+let num = document.getElementById('inp');
+// let sumOfDigits = 0;
+num.addEventListener('input', function(){
+     let enteredNum = this.value;
+     let digits = enteredNum.toString().split('');
+     let realDigits = digits.map(Number);
+     let sumOfDigits = 0;
+    //   console.log(realDigits.length);
+    for(let i=0; i<realDigits.length; i++){
+       sumOfDigits = sumOfDigits + realDigits[i];
+    }document.getElementById('sumOfDigits').innerHTML = `Sum of Digits = ${sumOfDigits}`;
+})
