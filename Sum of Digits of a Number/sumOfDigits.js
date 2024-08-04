b493ddc25d9c7f242d70fe194b569214fe321b32
Raka-schools <yrgreat2@gmail.com>
2024-08-04 15:10:14 +0530

    Number of Digits in a number

--- Sum of Digits of a Number/sumOfDigits.js
+++ Sum of Digits of a Number/sumOfDigits.js
@@ -1,12 +1,20 @@
-let num = document.getElementById('inp');
+let num = document.getElementById("inp");
+let realDigits;
 // let sumOfDigits = 0;
-num.addEventListener('input', function(){
-     let enteredNum = this.value;
-     let digits = enteredNum.toString().split('');
-     let realDigits = digits.map(Number);
-     let sumOfDigits = 0;
-    //   console.log(realDigits.length);
-    for(let i=0; i<realDigits.length; i++){
-       sumOfDigits = sumOfDigits + realDigits[i];
-    }document.getElementById('sumOfDigits').innerHTML = `Sum of Digits = ${sumOfDigits}`;
-})
+num.addEventListener("input", function () {
+  let enteredNum = this.value;
+  let digits = enteredNum.toString().split("");
+  let realDigits = digits.map(Number);
+  let sumOfDigits = 0;
+  //   console.log(realDigits.length);
+  for (let i = 0; i < realDigits.length; i++) {
+    sumOfDigits = sumOfDigits + realDigits[i];
+  }
+  document.getElementById(
+    "sumOfDigits"
+  ).innerHTML = `Sum of Digits = ${sumOfDigits}`;
+  document.getElementById(
+    "numberOfDigits"
+  ).innerHTML = `Number of Digits = ${realDigits.length}`;
+});
+
